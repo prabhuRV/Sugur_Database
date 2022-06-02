b02b40fd705547933ec1c6c9dce47611681e5553
prabhuRV <katravthprabhu151@gmail.com>
2022-06-02 18:05:19 +0530

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express=require("express")
 const app=express();
 const importData=require("./data.json")
+const flipcart=require("./Flipcart.json")
 const BodyCare=require("./BodyCare.json")
+const BestOffers=require("./BestOffers.json")
+const Breshes=require("./Breshes.json")
+const Eye=require("./Eye.json")
+const Face=require("./Face.json")
+const Lips=require("./Lips.json")
+const makeup=require("./makeup.json")
+const mask=require("./mask.json")
+const MerchStation=require("./MerchStation.json")
+const SkinCare=require("./SkinCare.json")
+const Trending=require("./Trending.json")
+
 let port=process.env.PORT || 8080
 
 app.get("/",(req,res)=>
@@ -14,8 +26,53 @@ app.get("/products",(req,res)=>
 })
 app.get("/cloths",(req,res)=>
 {
+    res.send(flipcart)
+})
+app.get("/BodyCare",(req,res)=>
+{
     res.send(BodyCare)
 })
+app.get("/BestOffers",(req,res)=>
+{
+    res.send(BestOffers)
+})
+app.get("/Breshes",(req,res)=>
+{
+    res.send(Breshes)
+})
+app.get("/Eye",(req,res)=>
+{
+    res.send(Eye)
+})
+app.get("/Face",(req,res)=>
+{
+    res.send(Face)
+})
+app.get("/Lips",(req,res)=>
+{
+    res.send(Lips)
+})
+app.get("/makeup",(req,res)=>
+{
+    res.send(makeup)
+})
+app.get("/mask",(req,res)=>
+{
+    res.send(mask)
+})
+app.get("/MerchStation",(req,res)=>
+{
+    res.send(MerchStation)
+})
+app.get("/SkinCare",(req,res)=>
+{
+    res.send(SkinCare)
+})
+app.get("/Trending",(req,res)=>
+{
+    res.send(Trending)
+})
+
 app.listen(port,()=>
 {
     console.log(`Exmple app is listening on port http://localhost:${port}`)
